b68c5befc643d685a63825abd9d871c5bcc5fef4
jaketrent <trent.jake@gmail.com>
2021-02-01 13:05:39 -0700

    bash aliases, yabai and fzf updates

--- bash/.fzf.bash
+++ bash/.fzf.bash
@@ -1,14 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */Users/jaketrent/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/jaketrent/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/jaketrent/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
-
+source "/Users/jaketrent/.fzf/shell/key-bindings.bash"
